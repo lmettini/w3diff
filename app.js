9add9d020c27294ee3ca7b4ca140d4e08fcfe0b0
Leandro Mettini <lea1987@gmail.com>
2014-07-14 00:53:57 -0300

    change port config

--- app.js
+++ app.js
@@ -13,11 +13,8 @@ var express = require('express')
 
 var app = express();
 
-var port =  process.env.PORT || 3000;
-
 app.configure(function(){
-  //app.set('port', process.env.PORT || 3000);
-  app.set('port', 3000);
+  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
