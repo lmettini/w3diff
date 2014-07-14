68b3df0c4e2ecb717e08de7151f7a9eb10767743
Leandro Mettini <lea1987@gmail.com>
2014-07-13 23:50:06 -0300

    add ping

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get("/ping", function(req, res) {res.send("PONG",200);});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
